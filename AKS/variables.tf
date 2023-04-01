4d99abeba9668cb4d402578e282dc22f4907d5cc
bespokeeagle <symbolchimere@gmail.com>
2023-04-01 22:05:06 +0300

    aks creation

---
+++ AKS/variables.tf
@@ -0,0 +1,33 @@
+variable "agent_count" {
+  default = 3
+}
+
+variable "cluster_name" {
+  default = "rama"
+}
+
+variable "dns_prefix" {
+  default = "rama"
+}
+# Refer to https://azure.microsoft.com/global-infrastructure/services/?products=monitor for available Log Analytics regions.variable "log_analytics_workspace_location" {  default = "eastus"}variable "log_analytics_workspace_name" {  default = "testLogAnalyticsWorkspaceName"}# Refer to https://azure.microsoft.com/pricing/details/monitor/ for Log Analytics pricingvariable "log_analytics_workspace_sku" {  default = "PerGB2018"}
+variable "resource_group_location" {
+  default     = "eastus"
+  description = "Location of the resource group."
+}
+
+variable "resource_group_name_prefix" {
+  default     = "rg"
+  description = "Prefix of the resource group name that's combined with a random ID so name is unique in your Azure subscription."
+}
+
+variable "ssh_public_key" {
+  default = "~/.ssh/id_rsa.pub"
+}
+
+variable "aks_service_principal_app_id" {
+  default = ""
+}
+
+variable "aks_service_principal_client_secret" {
+  default = ""
+}

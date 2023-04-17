91ab008e28272b909fff47d720559349000b080f
bespokeeagle <symbolchimere@gmail.com>
2023-04-17 19:17:33 +0300

    update

--- AKS/main.tf
+++ AKS/main.tf
@@ -5,7 +5,7 @@
 
 resource "azurerm_resource_group" "rg" {
   location = var.resource_group_location
-  name     = "${var.resource_group_name_prefix}"-"${var.name}"
+  name     =  "${var.resource_group_name_prefix}-${var.name}"
 }
 
 
